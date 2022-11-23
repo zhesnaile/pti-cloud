0b6478b817122a8900f22f849cf91d363603f514
jordiibru <jordibrucarci@gmail.com>
2022-11-23 17:54:18 +0100

    fix navbar

--- src/frontend-cloud/src/Components/Common/navbar/Navbar.jsx
+++ src/frontend-cloud/src/Components/Common/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import upc_fib from '../../Pics/logo_fib.png'
 function Navbar ({profile}) {
     return (
         <section className='navbar'>
-            {(profile === "") ? <a href='/login' className='navbar-item'></a> : <a href="/dashboard" className='navbar-item'>{profile}</a>}
+            {(profile === "") ? "" : <a href="/dashboard" className='navbar-item'>{profile}</a>}
             <a href="/dashboard" className="navbar-item">Home</a>
             <a href="https://docencia.ac.upc.es/FIB/grau/PTI/"className='navbar-item'> Projectes de TI</a>
             <section className='navbar-item'>
