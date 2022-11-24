7b6cd3d5c33dc89e119d5c1710f433da6cf7c5b0
jucorsan28 <101179614+jucorsan28@users.noreply.github.com>
2022-11-24 11:10:27 +0100

    [MOD] API registerNode

--- src/api/endpoints/registerNode.js
+++ src/api/endpoints/registerNode.js
@@ -1,6 +1,16 @@
 import { get_k3s_token } from "../../utils/get-token-k3s.js";
 
-async function 
+async function getToken_registerNode() {
+  let token = await get_k3s_token();
+  ctx.body = token;
+  ctx.status = 200;
+
+  console.log("Token guardado en memoria");
+
+  fs.appendFile('k3s_token.txt', token, function (err) {
+  if (err) throw err;
+
+}
 
 function init_registerNode_router() {
     let router = new KoaRouter();
--- src/utils/get-token-k3s.js
+++ src/utils/get-token-k3s.js
@@ -3,4 +3,5 @@ import {fs} from 'fs';
 export async function get_k3s_token() {
   let file = fs.readFileSync('/var/lib/rancher/k3s/server/node-token');
   return  file;
+  console.log("Token leído");
 }
