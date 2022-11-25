117f7948379b16392adfaa84d575a90ec8f0ec29
jordiibru <jordibrucarci@gmail.com>
2022-11-25 22:23:15 +0100

    Intent descarregar un fitxer
    la connexio amb la api va be i troba l'arxiu pero faltaria descarreglarlo?

--- backend/src/api/endpoints/registerNode.js
+++ backend/src/api/endpoints/registerNode.js
@@ -1,6 +1,9 @@
 import KoaRouter from "@koa/router";
 import KoaBodyParser from "koa-bodyparser";
 import { get_k3s_token } from "../../utils/get-token-k3s.js";
+import mime from "mime-types";
+import fs from "fs";
+import cors from "@koa/cors";
 
 async function getToken_registerNode() {
   let token = await get_k3s_token();
@@ -14,11 +17,26 @@ async function getToken_registerNode() {
   } );
 }
 
+
+async function getHello(ctx, next){
+  let path = "/Users/jordiibru/Documents/first-clone/pti-cloud/backend/public/hello.txt";
+  var mimeType = mime.lookup(path);
+  const src = fs.createReadStream(path);
+  ctx.response.set("content-type", mimeType);
+  ctx.response.set("content-disposition", "attachment; filename=hello.txt");
+  ctx.status = 200;
+  ctx.body = src;
+
+  await next();
+}
+
 function init_registerNode_router() {
     let router = new KoaRouter();
     router
+        .use(cors())
         .use(KoaBodyParser())
-        .post("/registerNode", getToken_registerNode);
+        .post("/registerNode", getToken_registerNode)
+        .post("/helloTest", getHello);
     return router;
 }
 
--- frontend/src/Components/Common/registernode/RegisterNode.jsx
+++ frontend/src/Components/Common/registernode/RegisterNode.jsx
@@ -16,8 +16,22 @@ function UserMenu({ Logout }) {
         Logout();
     }
 
-    const getScript = e => {
+    const getScript = async e => {
         //descarregar el script
+        e.preventDefault();
+        try {
+            let res = await fetch("http://localhost:3000/api/helloTest", {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+        });
+        if (res.status === 200) console.log("Funciona");
+        else console.log("No funciona encara");
+        } catch (err){
+            console.log(err);
+        }
     }
 
     const handle_K3S = (event) => {
@@ -34,8 +48,15 @@ function UserMenu({ Logout }) {
         //setWG_config(num);
     }
 
+    const K3S_change = event => {
+        setK3S_token(event.target.value);
+    }
+    const WG_change = event => {
+        setWG_config(event.target.value);
+    }
+
     return (
-        <form onSubmit={submitHandler}>
+        <form onSubmit={getScript}>
             <div className='main'>
                 <div className='sub-main-menu'>
                     <div className='content-menu'>
@@ -44,22 +65,22 @@ function UserMenu({ Logout }) {
                             <h3>Follow the instructions below to be able to register a node:</h3>
                             <div>
                                 <h4> &gt; Manually download the installer.</h4>
-                                <button className='classic-button' onClick={() => getScript()}>Download</button>
+                                <button type='submit' className='classic-button'>Download</button>
                                 <h4> &gt; To run this script, you must follow the next steps on your terminal:</h4>
                                 <p className='h4-node'>1. Enter the directory where you have downloaded the script</p>
                                 <p className='h4-node'>2. Modify the file permissions: chmod +x ./_myscript_.sh</p>
-                                <p className='h4-node'>3. Run the script: ./_myscript_sh</p>
+                                <p className='h4-node'>3. Run the script: ./_myscript_.sh</p>
                             </div>
                             <div>
                                 <h3>The following steps are optional:</h3> 
                                 <label> &gt; Get the hidden K3S token
-                                    <input className='checkbox-node' type="checkbox" onClick={handle_K3S} checken={K3S_flag}/>
-                                    <input className='input-node' value={K3S_flag ? K3S_token : '?'}/>                                    
+                                    <input className='checkbox-node' type="checkbox" onChange={handle_K3S} checked={K3S_flag}/>
+                                    <input className='input-node' onChange={K3S_change} value={K3S_flag ? K3S_token : '?'}/>                                    
                                 </label> 
                                 <br/>
                                 <label> &gt; Get the hidden Wireguard config
-                                    <input className='checkbox-node' type="checkbox" onClick={handle_WG} checken={WG_flag}/>
-                                    <input className='input-node' value={WG_flag ? WG_config : '?'}/>
+                                    <input className='checkbox-node' type="checkbox" onChange={handle_WG} checked={WG_flag}/>
+                                    <input className='input-node' onChange={WG_change} value={WG_flag ? WG_config : '?'} />
                                 </label>
                             </div>
                         </div>
