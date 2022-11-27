6c9fc06dbe52800a41a4e2df7637f3e393f797e0
jucorsan28 <101179614+jucorsan28@users.noreply.github.com>
2022-11-27 19:13:58 +0100

    [FIX] Obtención del token mediante get_Token()

--- backend/src/api/endpoints/registerNode.js
+++ backend/src/api/endpoints/registerNode.js
@@ -1,26 +1,34 @@
 import KoaRouter from "@koa/router";
 import KoaBodyParser from "koa-bodyparser";
-import { get_k3s_token } from "../../utils/get-token-k3s.js";
+import { get_k3s_token, get_Node_Name } from "../../utils/add-k3s-node.js";
 
-async function get_Token() {
+async function get_Token(ctx, next) {
   let token = await get_k3s_token();
-  ctx.body = token;
+  ctx.response = token;
   ctx.status = 200;
 
-  console.log("Token guardado en memoria");
+  console.log("Devuelve token");
 
+  await next();
 }
 
-async function get_NodeName() {
-  let name ;
+async function get_Name(ctx, next) {
+  let user = ctx.request.body.username;
+  let name = await get_Node_Name(user);
+  ctx.response = name;
+  ctx.status = 200;
+
+  console.log("Devuleve nombre");
+
+  await next();
 }
 
 function init_registerNode_router() {
     let router = new KoaRouter();
     router
         .use(KoaBodyParser())
-        .post("/getToken", get_Token);
-        .get("/getNodeName", get_Node_Name)
+        .get("/getToken", get_Token);
+        .get("/getNodeName", get_Name)
     return router;
 }
 
