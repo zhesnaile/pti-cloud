94664e906730a111d5f8e672fa94d0974e252b93
jucorsan28 <101179614+jucorsan28@users.noreply.github.com>
2022-11-27 16:07:09 +0100

    [MOD] routes and function names

--- backend/src/api/endpoints/registerNode.js
+++ backend/src/api/endpoints/registerNode.js
@@ -2,23 +2,25 @@ import KoaRouter from "@koa/router";
 import KoaBodyParser from "koa-bodyparser";
 import { get_k3s_token } from "../../utils/get-token-k3s.js";
 
-async function getToken_registerNode() {
+async function get_Token() {
   let token = await get_k3s_token();
   ctx.body = token;
   ctx.status = 200;
 
   console.log("Token guardado en memoria");
 
-  fs.appendFile('k3s_token.txt', token, function (err) {
-    if (err) throw err;
-  } );
+}
+
+async function get_NodeName() {
+  let name ;
 }
 
 function init_registerNode_router() {
     let router = new KoaRouter();
     router
         .use(KoaBodyParser())
-        .post("/registerNode", getToken_registerNode);
+        .post("/getToken", get_Token);
+        .get("/getNodeName", get_Node_Name)
     return router;
 }
 
