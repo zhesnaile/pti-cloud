647d3bec4805e81b8fb016b2a0c16102eb23b18f
jucorsan28 <101179614+jucorsan28@users.noreply.github.com>
2022-11-27 19:15:17 +0100

    [ADD] Función get_Node_Name para asignar nombres a los nodos

--- backend/src/utils/add-k3s-node.js
+++ backend/src/utils/add-k3s-node.js
@@ -2,6 +2,12 @@ import 'fs'
 
 export async function get_k3s_token() {
   let file = fs.readFileSync('/var/lib/rancher/k3s/server/node-token');
-  return  file;
   console.log("Token leído");
+  return  file;
+}
+
+export async function get_Node_Name(user) {
+  let name = user;
+  console.log("Nombre leído");
+  return name;
 }
