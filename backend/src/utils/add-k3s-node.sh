48e26d47eaff36585051fe6df36967015a8509c1
jucorsan28 <101179614+jucorsan28@users.noreply.github.com>
2022-11-27 19:14:46 +0100

    [IMP] Mejora del script para añadir nombre de los nodos

--- backend/src/utils/add-k3s-node.sh
+++ backend/src/utils/add-k3s-node.sh
@@ -1,15 +1,29 @@
 #!bin/bash
 
 # Install k3s script
-curl -sfL https://get.k3s.io | sh -
+#curl -sfL https://get.k3s.io | sh -
 
 # Check for Ready node, takes ~30 seconds
-k3s kubectl get node
+#k3s kubectl get node
 
 # Install
-sudo k3s agent --server https://myserver:6443 --token ${NODE_TOKEN}
+#sudo k3s agent --server https://myserver:6443 --token ${NODE_TOKEN}
 
+# Llama a la API para obtenr el TOKEN de k3s
 NODE_TOKEN=$(curl -L "${URL}/api/getToken")
 
+# Si hay un error en la llamada getToken, se detiene el Script
+if [[ $? -ne 0 ]]; then
+ 	echo -e "Something went wrong"
+fi
+
+# Se llama a la API para obetener el nombre del nodo
+NODE_NAME=$(curl -L "${URL}/api/getNodeName?username=${USERNAME}")
+
+# Si hay un error en la llamada getNodeName, se detiene el Script
+if [[ $? -ne 0 ]]; then
+ 	echo -e "Something went wrong. Maybe your user or password is incorrect"
+fi
+
 # Install k3s script and add node to URL cluster with TOKEN stored on /var/lib/rancher/k3s/server/node-token in the server
-# curl -sfL https://get.k3s.io | K3S_URL=$(URL):6443 K3S_TOKEN=${NODE_TOKEN} K3S_NODE_NAME=${NODE_NAME} sh -
+curl -sfL https://get.k3s.io | K3S_URL=${URL}:6443 K3S_TOKEN=${NODE_TOKEN} K3S_NODE_NAME=${NODE_NAME} sh -
