a46f9a1df649fd10ed03184212b2ddb3e3c1312a
jucorsan28 <101179614+jucorsan28@users.noreply.github.com>
2022-11-28 02:45:35 +0100

    [FIX] Acceso script al servidor

--- backend/src/utils/add-k3s-node.sh
+++ backend/src/utils/add-k3s-node.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 # Install k3s script
 #curl -sfL https://get.k3s.io | sh -
@@ -10,7 +10,7 @@
 #sudo k3s agent --server https://myserver:6443 --token ${NODE_TOKEN}
 
 # Llama a la API para obtenr el TOKEN de k3s
-read -rp "Server URL and Port:" -e -i https:// URL
+read -rp "Server URL and Port: " -e -i http:// URL
 NODE_TOKEN=$(curl -X GET "${URL}/api/getToken")
 
 # Si hay un error en la llamada getToken, se detiene el Script
@@ -19,15 +19,15 @@ if [[ $? -ne 0 ]]; then
 fi
 
 # Se llama a la API para obetener el nombre del nodo
-read -rp "Username:" -e -i Username USERNAME
-NODE_NAME=$(curl -X POST ${URL}/api/getNodeName -H "Content-Type: application/json" -d "{'username': ${USERNAME}}")
+read -rp "Username:" -e USERNAME
+NODE_NAME=$(curl -X POST ${URL}/api/getNodeName -H "Content-Type: application/json" -d '{"username":"'"${USERNAME}"'"}')
 
 # Si hay un error en la llamada getNodeName, se detiene el Script
 if [[ $? -ne 0 ]]; then
  	echo -e "Something went wrong. Maybe your user or password is incorrect"
 fi
 
-echo $(TOKEN)
-echo $(USERNAME)
+echo $NODE_TOKEN
+echo $NODE_NAME
 # Install k3s script and add node to URL cluster with TOKEN stored on /var/lib/rancher/k3s/server/node-token in the server
 #curl -sfL https://get.k3s.io | K3S_URL=${URL}:6443 K3S_TOKEN=${NODE_TOKEN} K3S_NODE_NAME=${NODE_NAME} sh -
