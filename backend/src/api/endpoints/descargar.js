5e06d353efec505fdf51484dff84c54068e977fb
jordiibru <jordibrucarci@gmail.com>
2022-11-30 22:41:27 +0100

    instalador funciona a la web

--- backend/src/api/endpoints/descargar.js
+++ backend/src/api/endpoints/descargar.js
@@ -2,19 +2,22 @@ import KoaRouter from "@koa/router";
 import KoaBodyParser from "koa-bodyparser";
 import mime from "mime-types";
 import fs from "fs";
+import path from "path";
 
 async function getScript(ctx, next){
-    let path = "./../../../public/installation-client.sh";
-    var mimeType = mime.lookup(path);
-    const src = fs.createReadStream(path);
+  //let file_path = "./installation-client.sh";
+  // let path = "/Users/jordiibru/Documents/first-clone/pti-cloud/backend/public/installation-client.sh";
+  const __dirname = path.resolve();
+  var file_path = path.join(__dirname, 'public', 'installation-client.sh');
+  var mimeType = mime.lookup(file_path);
+  const src = fs.createReadStream(file_path);
     
-    ctx.response.set("Content-disposition", "attachment; filename=installation-client.sh");
-    ctx.response.set("Content-type", mimeType);
-    //ctx.response.set("Content-Type", "application/force-download")
-    ctx.status = 200;
-    ctx.body = src;
+  ctx.response.set("Content-disposition", "attachment; filename=installation-client.sh");
+  ctx.response.set("Content-type", mimeType);
+  ctx.status = 200;
+  ctx.body = src;
   
-    await next();
+  await next();
   }
 
 
