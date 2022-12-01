32e2efd0bbc1129809bb4d7a946ae812eebcb7a2
jucorsan28 <101179614+jucorsan28@users.noreply.github.com>
2022-12-01 13:06:38 +0100

    [ADD] JSDoc to registerNode.js, add-k3s-node.js and get_wg_config.js

--- backend/src/api/endpoints/get_wg_config.js
+++ backend/src/api/endpoints/get_wg_config.js
@@ -4,6 +4,16 @@ import mime from "mime-types"
 import fs from 'fs'
 import { getConfig, deleteConfig } from "../../utils/access-wg.js";
 
+/**
+ * This file contains all the functions related to managing a node in the VPN structure (Wireguard server).
+ */
+
+ /**
+  * Obtains the configuration filename of the user and sends it to the client.
+  * Basically, it is called by the install-client.sh script via curl -X POST, so the configuration file can be transfered to the client.
+  * @param {*} ctx The context of the request. The context passed consists in: {username} (client username)
+  * @param {*} next
+  */
 async function get_wg_config(ctx, next) {
     ctx.status = 404;
     let user = ctx.request.body.username;
@@ -20,6 +30,13 @@ async function get_wg_config(ctx, next) {
     }
     await next();
 }
+
+/**
+ * Deletes the user in the DB and in the VPN network.
+ * Basically, it is called by the revoke-client.sh script via curl -X DELETE (when the administators call it), so the client can be deleted.
+ * @param {*} ctx The context of the request. The context passed consists in: {numb} (configuration number position on the VPN)
+ * @param {*} next
+ */
 async function revoke_wg_config(ctx, next) {
     ctx.status = 200;
     ctx.body = "ELIMINADO";
@@ -29,6 +46,10 @@ async function revoke_wg_config(ctx, next) {
     await next();
 }
 
+/**
+ * Router to make a collection of all the API functions in get_wg_config.js
+ * @returns The router with the implementation of the get_wg_config and revoke_wg_config functions.
+ */
 function init_wg_router() {
     let router = new KoaRouter();
     router
@@ -38,6 +59,7 @@ function init_wg_router() {
     return router;
 }
 
+/** Router collecting all functions of get_wg_config.js */
 export let wg_api_router = init_wg_router();
 //curl -X POST http://localhost:3000/api/wg_getConfig -H "Content-Type: application/json" -d '{"username": "jordi"}'
 //curl -X DELETE http://localhost:3000/api/wg_revoke -H "Content-Type: application/json" -d '{"numb": "2"}'
--- backend/src/api/endpoints/registerNode.js
+++ backend/src/api/endpoints/registerNode.js
@@ -4,10 +4,16 @@ import cors from "@koa/cors";
 import { get_k3s_token, get_Node_Name } from "../../utils/add-k3s-node.js";
 import { redis_get_K3Sconfig } from "../../utils/access-redis.js";
 
-/*
-  Comprobar funcionamiento:
-  curl -X GET http://localhost:3000/api/getToken
-*/
+/**
+ * This file contains all the functions related to adding a node in the K3s cluster.
+ */
+
+/**
+ * Obtains the K3s Token and passes it to the client.
+ * Basically, it is called by the install-client.sh script via curl -X GET, so the Token can be transfered to the client.
+ * @param {*} ctx The context of the request. Empty because we don't need any option in this GET.
+ * @param {*} next
+ */
 async function get_Token(ctx, next) {
   let token = await get_k3s_token();
 
@@ -18,10 +24,12 @@ async function get_Token(ctx, next) {
   await next();
 }
 
-/*
-Comprobar funcionamiento:
-curl -X POST http://localhost:3000/api/getNodeName -H "Content-Type: application/json" -d '{"user": "juan"}'
-*/
+/**
+ * Obtains a name for the node and assigns it to the client.
+ * Basically, it is called by the install-client.sh script, so the node obtains a name and it is assigned to the user.
+ * @param {*} ctx The context of the request. The context passed consists in: {username} (client username)
+ * @param {*} next
+ */
 async function get_Name(ctx, next) {
   let user = ctx.request.body.username;
   let name = await get_Node_Name(user);
@@ -33,7 +41,10 @@ async function get_Name(ctx, next) {
   await next();
 }
 
-
+/**
+ * Router to make a collection of all the API functions in registerNode.js
+ * @returns The router with the implementation of the get_Token and get_Name functions.
+ */
 function init_registerNode_router() {
     let router = new KoaRouter();
     router
@@ -44,4 +55,5 @@ function init_registerNode_router() {
     return router;
 }
 
+/** Router collecting all functions of registerNode.js */
 export let register_node_api_router = init_registerNode_router();
