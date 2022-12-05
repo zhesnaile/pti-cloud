5c9e8ec7eda2d2c8827839ba16234bc0bd8ab3eb
zhesnaile <carlosojar@gmail.com>
2022-12-05 13:30:17 +0100

    [FIX] Reemplazado tipo String con string.

--- backend/src/utils/access-redis.ts
+++ backend/src/utils/access-redis.ts
@@ -4,6 +4,7 @@
  * The code is divided in different sections -> WebApp, Wireguard and K3S.
  */
 import redis from 'redis';
+import { parse_int_from_string } from "./parsing-utils.js"
 
 //---- WEB APP ------------------------------------------------------------------------------------
 
@@ -15,7 +16,7 @@ import redis from 'redis';
  * @returns TRUE if credentials are valid and the user exists on the DB.
  * @returns FALSE if credentials are invalid (undefined) or the user does not exist on the DB.
  */
-export async function redis_login_user(user, password) {
+export async function redis_login_user(user: string , password: string) {
     if (user === undefined || password === undefined) {
         //console.log(`${Date.now()} LOGIN ERROR: Failed login`);
         return false;
@@ -73,7 +74,7 @@ export async function redis_register_user(user: string, password: string, passwo
  * @returns TRUE if the user exists in the DB.
  * @returns FALSE if the user does not exists in the DB.
  */
- export async function check_user(user) {
+ export async function check_user(user: string) {
     const redisClient = redis.createClient();
     await redisClient.connect();
     if (await redisClient.exists(user) !== 1){
@@ -95,7 +96,7 @@ export async function redis_register_user(user: string, password: string, passwo
  * @returns TRUE if the user exists in the DB and could add the new fields.
  * @returns FALSE if the user does not exists in the DB.
  */
-export async function redis_wgconfig(user, wg_num, wg_config) {
+export async function redis_wgconfig(user: string, wg_num:string , wg_config: string) {
     const redisClient = redis.createClient();
     await redisClient.connect();
 
@@ -115,15 +116,15 @@ export async function redis_wgconfig(user, wg_num, wg_config) {
 /**
  * Redis function for Wireguard based purposes.
  * Check if the user exists in the DB and gets the value of its wireguard config.
- * @param {String} user Username of the user that we want to check.
+ * @param {string} user Username of the user that we want to check.
  * @returns If the user exists and has a valid wg_config, returns the wg_config of the user in the DB.
  * @returns If the user does not exist, or has an invalid wg_config, or does not have any wg_config assigned, returns null.
  */
-export async function redis_get_wgconfig(user: String)){
+export async function redis_get_wgconfig(user: string){
     const redisClient = redis.createClient();
     await redisClient.connect();
-    let config = 'null';
-    if (await redisClient.exists(user) === 1 && await redisClient.hExists(user, 'wg_config') == 1 && redisClient.hGet(user,'wg_config') !== 'invalid') {
+    let config = null;
+    if (await redisClient.exists(user) === 1 && await redisClient.hExists(user, 'wg_config') == true && await redisClient.hGet(user,'wg_config') !== 'invalid') {
         config = await redisClient.hGet(user, 'wg_config');
     }
     await redisClient.disconnect();
@@ -133,16 +134,17 @@ export async function redis_get_wgconfig(user: String)){
 /**
  * Redis function for Wireguard based purposes.
  * Check if the user exists in the DB and gets the value of its wiregurd id (wg_num).
- * @param {String} user Username of the user that we want to check.
+ * @param {string} user Username of the user that we want to check.
  * @returns If the user exists and has a valid wg_num, returns the wg_num of the user in the DB.
  * @retuns If the user does not exist, or has an invalid wg_num, or does not have any wg_num assigned, returns null.
  */
- export async function redis_get_wgnum(user: String) : Promise<number>{
+ export async function redis_get_wgnum(user: string) : Promise<number | null>{
     const redisClient = redis.createClient();
     await redisClient.connect();
-    let wgnum = 'null';
-    if (await redisClient.exists(user) === 1 && await redisClient.hExists(user, 'wg_num') == 1 && redisClient.hGet(user,'wg_num') !== 'invalid'){
+    let wgnum = null;
+    if (await redisClient.exists(user) === 1 && await redisClient.hExists(user, 'wg_num') == true && await redisClient.hGet(user, 'wg_num') !== 'invalid'){
         wgnum = await redisClient.hGet(user, 'wg_num');
+        wgnum = parse_int_from_string(wgnum);
     }
     await redisClient.disconnect();
     return wgnum;
@@ -152,11 +154,11 @@ export async function redis_get_wgconfig(user: String)){
  * Redis function for Wireguard based purposes.
  * Check if the user exists in the DB and revokes its credentials.
  * wg_num and wg_config will change to "invalid".
- * @param {String} user Username of the user that we want to revoke.
+ * @param {string} user Username of the user that we want to revoke.
  * @returns TRUE if the user exists.
  * @returns FALSE if the user not exists.
  */
-export async function redis_revoke_wgconfig(user){
+export async function redis_revoke_wgconfig(user: string){
     const redisClient = redis.createClient();
     await redisClient.connect();
     if(await redisClient.exists(user) === 1){
@@ -174,12 +176,12 @@ export async function redis_revoke_wgconfig(user){
 /**
  * Redis function for K3S based purposes.
  * Check if the user exists in the DB and assign a k3s_name.
- * @param {String} user Username of the user that we want to assign a K3S machine.
- * @param {String} k3s_name Name of the K3S machine.
+ * @param {string} user Username of the user that we want to assign a K3S machine.
+ * @param {string} k3s_name Name of the K3S machine.
  * @returns TRUE if the user exists.
  * @returns FALSE if the user does not exists.
  */
-export async function redis_K3Sconfig(user: String, k3s_name: String) {
+export async function redis_K3Sconfig(user: string, k3s_name: string) {
     const redisClient = redis.createClient();
     await redisClient.connect();
 
@@ -197,15 +199,15 @@ export async function redis_K3Sconfig(user: String, k3s_name: String) {
 /**
  * Redis function for K3S based purposes.
  * Check if the user exists in the DB and checks if the k3s_names passed as a parameter is assigned to the user.
- * @param {String} user Username of the user that we want to check its k3s_name.
- * @param {String} k3s_name Name of a k3s machine.
+ * @param {string} user Username of the user that we want to check its k3s_name.
+ * @param {string} k3s_name Name of a k3s machine.
  * @returns TRUE if the user has the k3s_name assigned as a k3s_name.
  * @returns FALSE if the user does not have k3s_name assigned, or is invalid or the user does not exist.
  */
-export async function redis_check_K3Sconfig(user, k3s_name){
+export async function redis_check_K3Sconfig(user: string, k3s_name: string) : Promise<boolean>{
     const redisClient = redis.createClient();
     await redisClient.connect();
-    if (await redisClient.exists(user) === 1 && await redisClient.hExists(user, 'k3s_name') == 1 && await redisClient.hGet(user, 'k3s_name') == k3s_name){
+    if (await redisClient.exists(user) === 1 && await redisClient.hExists(user, 'k3s_name') == true && await redisClient.hGet(user, 'k3s_name') == k3s_name){
         redisClient.disconnect();
         return true;
     }
@@ -216,18 +218,19 @@ export async function redis_check_K3Sconfig(user, k3s_name){
 /**
  * Redis function for K3S based purposes.
  * Check if the user passed as a parameter has a k3s_name assigned and gets it.
- * @param {String} user Username of the user that we want to check its K3S_name.
+ * @param {string} user Username of the user that we want to check its K3S_name.
  * @returns the K3S_name of the user if it has one assigned.
  * @returns null if the user does not has any machine assigned or does not exist or is invalid.
  */
-export async function redis_get_K3Sconfig(user){
+export async function redis_get_K3Sconfig(user: string): Promise<string | null> {
     const redisClient = redis.createClient();
     await redisClient.connect();
-    let k3s_name = 'null';
-    if (await redisClient.exists(user) === 1 && await redisClient.hExists(user, 'k3s_name') == 1 && redisClient.hGet(user,'k3s_name') !== 'invalid'){
+    let k3s_name = null;
+    if (await redisClient.exists(user) === 1 && await redisClient.hExists(user, 'k3s_name') == true && await redisClient.hGet(user,'k3s_name') !== 'invalid'){
         k3s_name = await redisClient.hGet(user, 'k3s_name');
     }
     await redisClient.disconnect();
+    if (k3s_name == undefined) k3s_name = null;
     return k3s_name;
 }
 
