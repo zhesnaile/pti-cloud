54d22543c1dadcedfd6eaf2ae01e609070ab531e
jucorsan28 <101179614+jucorsan28@users.noreply.github.com>
2022-12-13 01:10:17 +0100

    [FIX] Obtención de parámetros desde ctx

--- backend/src/api/endpoints/runTask.ts
+++ backend/src/api/endpoints/runTask.ts
@@ -7,59 +7,67 @@ import KoaRouter from "@koa/router";
 import KoaBodyParser from "koa-bodyparser";
 import { loginBody } from "../../types/api_request_bodies.js"
 
+/**
+ * This file contains all the functions related to executing a service in the K3s cluster.
+ */
 
-async function upload_yaml (ctx: ParameterizedContext, next: Next) { 
-    let req_body = <loginBody>ctx.request.body;
-
-    let valid_login = await redis_login_user(req_body.name, req_body.pword);
-    if (valid_login === true) {
-        ctx.status = 200;
-        ctx.body = `Welcome back, ${req_body.name}.`;
-    } else {
-        ctx.status = 404;
-        ctx.body = `Login error.`;
-        await next();
-    }
-
-    let file = ctx.request.body.file;
-
-    if (file !== null) {
-        let valid_extension = ['yaml', 'yml'];
-        let file_name = file.name;
-        let file_extension = file_name.split('.').pop();
-        let valid = valid_extension.includes(file_extension);
-        if (valid === false) {
-            ctx.status = 404;
-            ctx.body = 'File extension not valid.';
-        }
-    } else {
-        ctx.status = 404;
-        ctx.body = 'Error.';
-        await next();
-    }
-
-    //formidable: {uploadDir: '/var/lib/rancher/k3s/server/manifests' },
-   
+/**
+ * Obtains the K3s Token and passes it to the client.
+ * Basically, it is called by the install-client.sh script via curl -X GET, so the Token can be transfered to the client.
+ * @param {*} ctx The context of the request. Empty because we don't need any option in this GET.
+ * @param {*} next
+ */
+async function upload_yaml (ctx: ParameterizedContext, next: Next) {
+  //let req_body = <loginBody>ctx.request.body;
+  let valid_login = await redis_login_user(ctx.request.body.name, ctx.request.body.pword);
+  if (valid_login === true) {
+    ctx.status = 200;
+    ctx.body = `Welcome back, ${ctx.request.body.name}.`;
+  } else {
+    ctx.status = 404;
+    ctx.body = `Login error.`;
     await next();
   }
-      //formidable: {uploadDir: '/var/lib/rancher/k3s/server/manifests' },
+
+  let file = ctx.request.body.file;
+
+  if (file !== null) {
+    let valid_extension = ['yaml', 'yml'];
+    let file_name = file.name;
+    let file_extension = file_name.split('.').pop();
+    let valid = valid_extension.includes(file_extension);
+    if (valid === false) {
+      ctx.status = 404;
+      ctx.body = 'File extension not valid.';
+      await next();
+    }
+  } else {
+    ctx.status = 404;
+    ctx.body = 'Error.';
+    await next();
+  }
+
+  //formidable: {uploadDir: '/var/lib/rancher/k3s/server/manifests' },
+   
+  await next();
+}
 
   
-  /**
-   * Router to make a collection of all the API functions in registerNode.js
-   * @returns The router with the implementation of the get_Token and get_Name functions.
-   */
-  function init_registerTask_router(): KoaRouter {
-      let router = new KoaRouter();
-      router
-        .use(KoaBodyParser({
-          formidable: {uploadDir: './uploads' },
-          multipart: true,
-          urlencoded: true
-        }))
-        .post("/uploadYAML", upload_yaml)
-      return router;
-  }
+/**
+* Router to make a collection of all the API functions in registerNode.js
+* @returns The router with the implementation of the get_Token and get_Name functions.
+*/
+function init_registerTask_router(): KoaRouter {
+  let router = new KoaRouter();
+  router
+    .use(KoaBodyParser({
+      formidable: {uploadDir: './uploads' },
+      multipart: true,
+      urlencoded: true
+    }))
+    .post("/uploadYAML", upload_yaml)
+  return router;
+}
   
-  /** Router collecting all functions of registerNode.js */
-  export let register_task_api_router = init_registerTask_router();  
+/** Router collecting all functions of registerNode.js */
+export let register_task_api_router = init_registerTask_router();  
