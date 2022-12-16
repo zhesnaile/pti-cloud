b9168d74714bb0f83901c0d589ce036adb0aadd0
jordiibru <jordibrucarci@gmail.com>
2022-12-16 22:45:42 +0100

    [fix] usermenu monitoring removed

--- frontend/src/Components/Common/usermenu/UserMenu.jsx
+++ frontend/src/Components/Common/usermenu/UserMenu.jsx
@@ -13,21 +13,21 @@ function UserMenu() {
                         <h1>USER MENU</h1>
                         <div className='row'>
                             <div className='column-menu'>
-                                <h3>Register node</h3>
+                                <h3 className='title-cols'>Register node</h3>
                                 <p className='p-menu'>This request will give a K3S token to the user in order to successfully register a node in the cluster.</p>
                                 <button className='button-menu' type='submit' onClick={() => navigate('/registerusernode')}>Register</button>
                             </div>
                             <div className='column-menu'>
-                                <h3>Run job</h3>
+                                <h3 className='title-cols'>Run job</h3>
                                 <p className='p-menu'>This functionality will allow requesting resources to be able to run docker containers in the cluster.</p>
                                 <button className='button-menu' type='submit' onClick={() => navigate('/runjob')}>Run</button>
                             </div>
-                            <div className='column-menu'>
+                            {/*<div className='column-menu'>
                                 <h3>Monitoring</h3>
                                 <p className='p-menu'>Page to monitor services</p>
                                 <br/><br/>
                                 <button className='button-menu' type='submit' onClick={() => navigate('/login')}>Dashboard</button>
-                            </div>
+    </div>*/}
                         </div>                  
                     </div>
                 </div>
--- frontend/src/Components/ProtectedRoutes.jsx
+++ frontend/src/Components/ProtectedRoutes.jsx
@@ -7,7 +7,7 @@ function ProtectedRoutes({Component, Auth, Logout }) {
 
     return (
         <div>
-            {(Auth === false && Component === 'UserMenu') ? (<ErrorPage Logout={Logout}/>) : <UserMenu />}         
+            {(Auth === true && Component === 'UserMenu') ? (<ErrorPage Logout={Logout}/>) : <UserMenu />}         
         </div>
         
     )
