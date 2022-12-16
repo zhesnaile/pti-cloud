12c74c20b7cfcc20b3040b5237b9a4dc7904dbde
jucorsan28 <101179614+jucorsan28@users.noreply.github.com>
2022-12-16 22:30:25 +0100

    [MOD] Cambio orden instalador servidor

--- backend/public/install-server.sh
+++ backend/public/install-server.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Install redis
-sudo apt-get install redis
-npm i redis
-
 # Install K3s
 curl -sfL https://get.k3s.io | sh -
 
@@ -17,15 +13,24 @@ sudo k3s kubectl get node
 sudo chmod go+rx /var/lib/rancher/k3s/server
 sudo chmod go+rx /var/lib/rancher/k3s/server/node-token
 
-# Install dependencies of the project
+# Install redis
+sudo apt-get install redis
+npm i redis
+
+# Install dependencies of the project (in both directories)
 npm install
+npm install typescript
+npm install koa
+npm install @koa/multer
 
 # Install node version 16
-
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+node install 16
 
 # Compile the project
 npx tsc
 
 # Run the server
 npm run start
-
