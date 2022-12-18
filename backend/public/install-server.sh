bf3826fbd991288d119811ac1eb78021e25b32a9
jucorsan28 <101179614+jucorsan28@users.noreply.github.com>
2022-12-18 19:35:52 +0100

    [FIX] & redis y build del frontend

--- backend/public/install-server.sh
+++ backend/public/install-server.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # K3S INSTALLATION
-# 
 # Installation and configurations commands for the MASTER of the  K3s cluster.
-#
 
 # Download and installation
 curl -sfL https://get.k3s.io | sh -
@@ -19,25 +17,18 @@ sudo chmod go+rx /var/lib/rancher/k3s/server
 sudo chmod go+rx /var/lib/rancher/k3s/server/node-token
 
 # REDIS INSTALLATION
-#
-# Installation and execution commands 
-#
+# Installation and execution commands
 
 # Install redis
 sudo apt-get install redis
 sudo service redis-server stop
-redis-server
-
-# Install dpendencies of the project FRONTEND
-npm install
+redis-server &
 
 # Install node version 16
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install 16
-
-npm run build
 
 # Install dependencies of the project BACKEND
 npm install
