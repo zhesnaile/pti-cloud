1c0803c98a3c6e7dc77e9d75210efcf5f8a6b400
jucorsan28 <101179614+jucorsan28@users.noreply.github.com>
2022-12-18 17:34:41 +0100

    [FIX] Comentada función Wireguard

--- backend/public/installation-client.sh
+++ backend/public/installation-client.sh
@@ -64,35 +64,33 @@
 
 
 function installK3S {
+	#LLAMAR A LA API PARA QUE DE EL ARCHIVO DE CONFIG
+
+    read -rp "Server URL (without http:// or https://): " -e URL
+    read -rp "Username: " -e USERNAME
+
+    #SI HAY UN ERROR EN LA LLAMADA GETCONFIG DETEN EL SCRIPT
+    if [[ $? -ne 0 ]]; then
+        echo -e "Something went wrong. Maybe your user or password is incorrect"
+    fi
+
 	# Llama a la API para obtenr el TOKEN de k3s (para pner texto preescrito usar la oción -i de read)
 	# read -rp "Server URL (without http:// or https://): " -e URL
 	echo "Getting k3s Token:"
-	NODE_TOKEN=$(curl -X GET "http://${URL}:3000/api/getToken")
+	NODE_TOKEN=$(curl -X POST "http://${URL}:3000/api/getToken")
+	echo "${NODE_TOKEN}"
 
 	# Si hay un error en la llamada getToken, se detiene el Script
 	if [[ $? -ne 0 ]]; then
 	 	echo -e "Something went wrong"
 	fi
 
-	# Se llama a la API para obetener el nombre del nodo
-	#read -rp "Username:" -e USERNAME
-	echo "Getting Node Name:"
-	NODE_NAME=$(curl -X POST http://${URL}:3000/api/getNodeName -H "Content-Type: application/json" -d '{"username":"'"${USERNAME}"'"}')
-
-	# Si hay un error en la llamada getNodeName, se detiene el Script
-	if [[ $? -ne 0 ]]; then
-	 	echo -e "Something went wrong. Maybe your user or password is incorrect"
-	fi
-
-	echo "Installing k3s:"
-	export K3S_TOKEN=${NODE_TOKEN}
-	export K3S_URL="https://${URL}:6443"
-	export K3S_NODE_NAME=${NODE_NAME}
+	echo "Installing K3s worker node:"
 
 	# Install k3s script and add node to URL cluster with TOKEN stored on /var/lib/rancher/k3s/server/node-token in the server
-	curl -sfL https://get.k3s.io | sh -
+	curl -sfL https://get.k3s.io | K3S_URL=https://${URL}:6443 K3S_TOKEN=${NODE_TOKEN} sh -
 }
 
 isRoot
-installWireGuard
+# installWireGuard
 installK3S
